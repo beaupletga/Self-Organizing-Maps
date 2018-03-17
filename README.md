471bbffae8f13023fb2cb009c0e73723cec32b30
Gabriel Beauplet <beaupletga@eisti.eu>
2018-03-17 14:52:34 +0100

    Comment the code + update README

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Self-Organizing-Maps
 
+A self-organizing map (SOM) or self-organizing feature map (SOFM) is a type of artificial neural network (ANN) that is trained using unsupervised learning to produce a low-dimensional (typically two-dimensional), discretized representation of the input space of the training samples, called a map, and is therefore a method to do dimensionality reduction. Self-organizing maps differ from other artificial neural networks as they apply competitive learning as opposed to error-correction learning (such as backpropagation with gradient descent), and in the sense that they use a neighborhood function to preserve the topological properties of the input space. (source : Wikipédia)
 
-Here is an example when using SOM on colors 
+Here is the explanations of the algorithm.
+![](SOM_algorithm.png)
+
+
+Here is an example when using SOM on colors (it's super easy to visualize)
 
 ![](color_figure.png)
+
+
+
+
+
--- main.pyx
+++ main.pyx
@@ -5,29 +5,35 @@ map_size=[20,20]
 learning_rate=0.1
 iterations=2000
 
+
+#compute the neighboorhood value
+#we only want to update the close neurons
 cdef neighborhood_function(s,r,to=1):
     return np.exp(-np.linalg.norm(np.array(s)-np.array(r))/2*to)
 
-
+#compute the distance between one sample of the dataset X and the neurons of the som
+#return the place of the closest neuron's SOM
 def min_distance(xi,som):
     tmp=np.where(np.linalg.norm(xi-som,axis=-1)==np.min(np.linalg.norm(xi-som,axis=-1)))
     min_x=tmp[0][0]
     min_y=tmp[1][0]
     return [min_x,min_y]
 
-
+#update the neurons to be closer of the data sample
 def apply_update(som,place,xi): 
-    a=-som+xi
     cdef int i=0,j=0;
-    # cdef float distance=0;
     for i in range(som.shape[0]):
         for j in range(som.shape[1]):
+            #compute the distance between one sample of X (xi) and the neuron[i,j]
             distance=xi-som[i,j]
+            #update this neuron in order to be closer of xi
             som[i,j]+=learning_rate*neighborhood_function(place,[i,j],1)*distance
 
 def main(X,som,int iterations):
     cdef int i=0;
     cdef int j=0;
+    #for each iteration, we get through all the dataset
+    #and update the SOM neuron's for each sample
     for i in range(iterations):
         for j in range(X.shape[0]):
             place=min_distance(X[j],som)
